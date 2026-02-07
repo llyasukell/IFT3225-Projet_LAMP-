1b3e246578bc64e61871be4e6db04ea1ced2d6cc
Mamour Ndiaye <Mamourndiaye123@gmail.com>
2026-02-07 11:40:06 -0500

    ajout image
    image ajouter pour les tuiles

--- tuiles.js
+++ tuiles.js
@@ -1,10 +1,10 @@
 
 const tuilesData = [
-  { titre: "Service 1", lien: "#" },
-  { titre: "Service 2", lien: "#" },
-  { titre: "Service 3", lien: "#" },
-  { titre: "Service 4", lien: "#" },
-  { titre: "Service 5", lien: "#" }
+  { titre: "Service 1", lien: "#",image: "img/service1.png" },
+  { titre: "Service 2", lien: "#",image: "img/service1.png" },
+  { titre: "Service 3", lien: "#",image: "img/service1.png" },
+  { titre: "Service 4", lien: "#",image: "img/service1.png" },
+  { titre: "Service 5", lien: "#",image: "img/service1.png" }
 ];
 
 const grilleTuiles = document.getElementById("grille-tuiles");
@@ -18,14 +18,23 @@ tuilesData.forEach(service => {
   const h4 = document.createElement("h4");
   h4.textContent = service.titre;
 
+  const img = document.createElement("img");
+   img.src = service.image; 
+   img.alt = service.titre
+   img.className = "image-tuile";
+
+
 
   const btn = document.createElement("button");
+  
+  
+
   btn.textContent = "Voir";
   btn.onclick = () => {
     window.location.href = service.lien;
   };
 
-
+  tuile.appendChild(img);
   tuile.appendChild(h4);
   tuile.appendChild(btn);
   grilleTuiles.appendChild(tuile);
